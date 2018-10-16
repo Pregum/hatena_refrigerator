c429f5f66d5f4193896efd6d5ba312236bef0e91
Pregum <pregum.y@gmail.com>
2018-10-17 04:53:09 +0900

    [add] FoodとFoodViewModelの追加とSystem.Runtime.CompilersServices.UnsafeとSystem.ValueTupleのバージョンの更新

---
+++ Model/Food.cs
@@ -0,0 +1,74 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Windows.Media.Imaging;
+
+namespace Wpf_FoodManager.Model
+{
+    public class Food : BindableBase
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        private uint _id;
+        public uint Id
+        {
+            get { return _id; }
+            set { this.SetProperty(ref _id, value); }
+        }
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { this.SetProperty(ref _name, value); }
+        }
+
+        /// <summary>
+        /// 賞味期限
+        /// </summary>
+        private DateTime _limitDate;
+        public DateTime LimitDate
+        {
+            get { return _limitDate; }
+            set { this.SetProperty(ref _limitDate, value); }
+        }
+
+        /// <summary>
+        /// 購入日
+        /// </summary>
+        private DateTime _boughtDate;
+        public DateTime BoughtDate
+        {
+            get { return _boughtDate; }
+            set { this.SetProperty(ref _boughtDate, value); }
+        }
+
+        /// <summary>
+        /// 画像
+        /// </summary>
+        private BitmapImage _image;
+        public BitmapImage Image
+        {
+            get { return _image; }
+            set { this.SetProperty(ref _image, value); }
+        }
+
+        private static uint _countId = 1;
+
+        public Food()
+        {
+        }
+
+        public Food(string name, DateTime limitDate, DateTime boughtDate, BitmapImage image)
+        {
+            this._id = Food._countId++;
+            this._name = name;
+            this._limitDate = limitDate;
+            this._boughtDate = boughtDate;
+            this._image = image;
+        }
+    }
+}
---
+++ ViewModel/FoodViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Reactive.Disposables;
+using System.Windows.Media.Imaging;
+using Wpf_FoodManager.Model;
+
+namespace Wpf_FoodManager.ViewModel
+{
+    public class FoodViewModel : BindableBase, IDisposable
+    {
+        // ReactiveProperty<T>にすることでTの型のデータの通知受け取り、通知発行の役割を持ったプロパティになります。
+        public ReactiveProperty<uint> Id { get; }
+        public ReactiveProperty<string> Name { get; }
+        public ReactiveProperty<DateTime> LimitDate { get; }
+        public ReactiveProperty<DateTime> BoughtDate { get; }
+        public ReactiveProperty<BitmapImage> Image { get; }
+
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+
+        // test用
+        public FoodViewModel() : this(new Food("test1", new DateTime(2018, 10,17), new DateTime(2018, 10,19), new BitmapImage(new Uri("nothing", UriKind.RelativeOrAbsolute))))
+        {
+        }
+
+        public FoodViewModel(Food food)
+        {
+            this.Id = food.ObserveProperty(x => x.Id).ToReactiveProperty().AddTo(this.Disposable);
+            this.Name = food.ObserveProperty(x => x.Name).ToReactiveProperty().AddTo(this.Disposable);
+            this.LimitDate = food.ObserveProperty(x => x.LimitDate).ToReactiveProperty().AddTo(this.Disposable);
+            this.BoughtDate = food.ObserveProperty(x => x.BoughtDate).ToReactiveProperty().AddTo(this.Disposable);
+            this.Image = food.ObserveProperty(x => x.Image).ToReactiveProperty().AddTo(this.Disposable);
+        }
+
+        public void Dispose()
+        {
+            this.Disposable.Dispose();
+        }
+    }
+}
