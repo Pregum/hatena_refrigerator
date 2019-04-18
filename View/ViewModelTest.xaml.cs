975da314c3da3bf372cbaf2d51495cdc5d9c3773
Pregum <pregum.y@gmail.com>
2019-04-19 07:12:54 +0900

    [fix]ViewModelTestの修正

--- View/ViewModelTest.xaml.cs
+++ View/ViewModelTest.xaml.cs
@@ -11,17 +11,14 @@ namespace Wpf_FoodManager.View
     /// </summary>
     public partial class ViewModelTest : Window
     {
-#if _Single
-
         /// <summary>
         /// Model
         /// </summary>
-        //private Model.Food _foodModel;
+        private Model.Food _foodModel;
         /// <summary>
         /// ViewModel
         /// </summary>
-        //public ViewModel.FoodViewModel FoodViewModel { get; set; }
-#endif
+        public ViewModel.FoodViewModel FoodViewModel { get; set; }
 
         private FoodCollection _foodModelCollection;
 
@@ -32,20 +29,14 @@ namespace Wpf_FoodManager.View
             InitializeComponent();
 
             var ram = new Random();
-#if _Single
             _foodModel = new Model.Food("秋刀魚", new DateTime(2019, 4, 20), new DateTime(2019, 4, 27), new BitmapImage());
             FoodViewModel = new ViewModel.FoodViewModel(_foodModel);
             this.DataContext = FoodViewModel;
-#endif
-            var foodModels = Enumerable.Range(1, 10).Select(x => new Food("秋刀魚" + x, new DateTime(2019, 2, ram.Next(1, 10)), new DateTime(2019, 2, ram.Next(10, 20)), new BitmapImage()));
-            IFoodContainer foodContainer = new FileFoodContainer(foodModels);
-            this._foodModelCollection = new FoodCollection(foodContainer);
-            this.FoodViewModelCollection = new ViewModel.FoodViewModelCollection(this._foodModelCollection);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //this._foodModel.Name = "変更しました！";
+            this._foodModel.Name = "変更しました！";
         }
     }
 }
